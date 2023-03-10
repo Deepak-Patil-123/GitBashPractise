f87b94a4c559c912904a33cc7c26c392914bf244
Deepak-Patil-123 <patilrhdeepak@gmail.com>
2023-03-10 15:10:27 +0530

    update changes

---
+++ src/test/java/com/gitPractise/JenkinPractiseTest2.java
@@ -0,0 +1,18 @@
+package com.gitPractise;
+
+import org.testng.annotations.Test;
+
+public class JenkinPractiseTest2 {
+
+	@Test
+	public void test1()
+	{
+		System.out.println("class2--->test2");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("class2--->test2");
+	}
+	
+}
